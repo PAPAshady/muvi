dcee7433b630128e5c899c3dea9169f114f092bf
papashady <zamani.nima18@gmail.com>
2024-07-11 16:13:07 +0430

    added video js hotkeys plugin to control the video player with keyboard

--- assets/JS/pages/video-page.js
+++ assets/JS/pages/video-page.js
@@ -4,4 +4,11 @@ const player = videojs(video, {
     controls : true,
     playbackRates : [0.5, 1, 1.25, 1.5, 2, 4],
     inactivityTimeout : 3000,
+
+    plugins : {
+        hotkeys : {
+            volumeStep : 0.1,
+            seekStep : 5,
+        }
+    }
 })
